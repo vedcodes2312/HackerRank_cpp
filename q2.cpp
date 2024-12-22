cd64999c2c8ab5562a6cfc9e867ba24418fcfbf0
Ved Vyas <vedvyascodes1608@gmail.com>
2024-12-22 11:07:51 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,20 @@
+//Input and Output
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+ {
+    int a,b,c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    int sum = a+b+c;
+    cout<<sum;   
+    return 0;
+}
