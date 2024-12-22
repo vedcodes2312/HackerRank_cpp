7f8b45032acceeec4cf4123436156312d5e6e70d
Ved Vyas <vedvyascodes1608@gmail.com>
2024-12-22 11:14:17 +0530

    Create q3.cpp

---
+++ q3.cpp
@@ -0,0 +1,15 @@
+//basic data types
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e; 
+    cin >> a >> b >> c >> d >> e;
+    printf("%d\n%ld\n%c\n%f\n%lf\n", a, b, c, d, e);
+    return 0;
+}
